6ade187cc30e166935a4eb5c243d6243a0c526bf
rachel-learns <rachel.learns.projects@gmail.com>
2026-09-19 15:12:43 +0200

    Write tests in the MainTest.java file

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -2,12 +2,37 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MainTest {
     OneGame game = new OneGame();
 
     @Test
-    public void generateRandomNumberTest() {
-        game.generateRandomNumber(0,100);
+    public void getScoreTest() {
+        game.setScore(50);
+        assertEquals(50, game.getScore());
+    }
 
+    @Test
+    public void generateRandomNumberTest() {
+        int number = game.generateRandomNumber(0, 100);
+        assertTrue(number >= 0 && number <= 100);
+    }
+
+    @Test
+    public void compareNumbersTest() {
+        assertEquals(-1, game.compareNumbers(10, 20));
+        assertEquals(1, game.compareNumbers(20, 10));
+        assertEquals(0, game.compareNumbers(10, 10));
+    }
+
+    @Test
+    public void scoreOneGameTest() {
+        int score = game.scoreOneGame(4);
+        assertEquals(10, score);
+
+        int score1 = game.scoreOneGame(2);
+        assertEquals(2, score1);
     }
 }
