6f1b3e99177c2ea850bf0c286ad93fe52be89cce
rachel-learns <rachel.learns.projects@gmail.com>
2026-09-19 13:02:15 +0200

    Modularisation of the Main.java file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,58 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+
+    public static int generateRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(min, max);
+    }
+
+
+    public static int userInput() {
+        int number;
+        do {
+            System.out.println("Please enter a number between 0 and 100 : ");
+            Scanner scanner = new Scanner(System.in);
+            number = scanner.nextInt();
+        } while(number < 0 || number > 100);
+
+        return number;
+    }
+
+
+    public static int compareNumbers(int number, int solutionNumber) {
+        if(number == solutionNumber) {
+            return 0;
+        }
+
+        if(solutionNumber > number) {
+            return -1;
+        }
+
+        return 1;
+    }
+
+
+    public static int scoreOneGame(int nbrEssais) {
+        // Return the score of this game
+        int score;
+        if(nbrEssais == 5) {
+            score = 10;
+        } else {
+            score = nbrEssais;
+        }
+
+        return score;
+    }
+
+
     public static void main(String[] args) {
         boolean wantsToPlay = true;
         int gameMax = 10;
         int finalScore = 0;
 
         do {
-
             gameMax--;
 
             // The rules
@@ -23,41 +68,36 @@ public class Main {
             System.out.println();
 
             // Random generation of a number between 0 and 100
-            Random rand = new Random();
-            int solutionNumber = rand.nextInt(0, 100);
-            System.out.println(solutionNumber);
+            int solutionNumber = generateRandomNumber(0, 100);
+            //System.out.println(solutionNumber);
 
-            // Variable which indicates if the number has been found
+            // Variables initializations
             boolean numberIsFind = false;
             int nbrEssais = 5;
             int number;
 
-            // The game, continue until the number is found
+            // The game continue until the number is found or until there is no attempts left
             while (!numberIsFind && nbrEssais > 0) {
+                // The user enters a number
+                number = userInput();
+                nbrEssais--;
 
-                // User input
-                do {
-                    System.out.println("Please enter a number between 0 and 100 : ");
-                    Scanner scanner = new Scanner(System.in);
-                    number = scanner.nextInt();
-                } while (number < 0 || number > 100);
+                // The program compares the user's test with the solution
+                int hint = compareNumbers(number, solutionNumber);
 
-                if (number == solutionNumber) {
+                // We display a hint to help the user find the solution number
+                if (hint == 0) {
                     numberIsFind = true;
                 } else {
-                    nbrEssais--;
-
-                    if (solutionNumber > number) {
-                        System.out.println("The solution number is greater than the number.");
-                    }
-                    if (solutionNumber < number) {
-                        System.out.println("The solution number is lesser than the number.");
+                    if (hint == -1) {
+                        System.out.println("The solution number is bigger than the number.");
+                    } else {
+                        System.out.println("The solution number is smaller than the number.");
                     }
 
                     System.out.println("Try again. You have " + nbrEssais + " attempts left.");
                     System.out.println();
                 }
-
             }
 
             // Message of victory or loss
@@ -67,17 +107,12 @@ public class Main {
                 System.out.println("The number is not found! You lose! It was " + solutionNumber + ".");
             }
 
-            // Print the score of this game
-            int score;
-            if(nbrEssais == 5) {
-                score = 10;
-            } else {
-                score = nbrEssais;
-            }
+            // We display the score of this game
+            int score = scoreOneGame(nbrEssais);
             System.out.println("Your score is " + score + " for this game.");
             System.out.println();
 
-            // Keep track of the score
+            // Keep track of the final score
             finalScore = finalScore + score;
 
             // Ask the user if he wants to keep playing
@@ -89,7 +124,7 @@ public class Main {
             } while(!c.equals("y") && !c.equals("n"));
             System.out.println();
 
-            // If not, the game stops and the final score is printed
+            // If not, the game stops
             if(c.equals("n")) { wantsToPlay = false; }
 
         } while(wantsToPlay && gameMax > 0);
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 public class MainTest {
     @Test
     void mainTest() {
+        Main test1 = new Main();
 
     }
 }
